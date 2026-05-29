d19682cc58a2bd9e069deb73b9ab12aa75fe174e
Alex Jaime Vasquez Valderrama <alexiotovv@email.com>
2026-05-29 10:14:22 -0500

    errors_return_fixed

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,5 @@ Route::get('/horas-libres', [HomeController::class, 'horaslibres'])->name('horas
 
 // Ruta de fallback para SPA o página 404 personalizada
 Route::fallback(function () {
-    // return view('errors.404');
-    abort(404);
+    return view('errors.404');
 });
